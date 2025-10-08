7548f50de5a1ef3e8fc95febff9617dbe807ec30
PigeonTheGreat <masseygirl@outlook.ie>
2025-10-08 11:38:38 +0100

    FirstCommit

---
+++ Halloween Horrors/Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{       
+    
+    Rigidbody2D rbody;
+    
+    void Awake() //called when a gameobject is initialised, initialising rigidbody2d
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    
+
+    // Update is called once per frame
+    public void Launch (Vector2 direction, float force)
+    {
+        rbody.AddForce(direction * force);   
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
